f94b0310529d4cbb144db6c6e5a4e91d6dea7224
Retr0 <azamatalihamroev3@gmail.com>
2026-05-20 18:20:06 +0500

    Added HTTP codes on auth file

--- auth.py
+++ auth.py
@@ -7,15 +7,15 @@ SECRET_KEY=os.getenv("SECRET_KEY")
 def get_user_id():
     auth=request.headers.get("Authorization")
     if not auth:
-        return jsonify({"error":"no token"})
+        return jsonify({"error":"no token"}), 401
     elif not auth.startswith("Bearer "):
-        return jsonify({"error": "invalid token"})
+        return jsonify({"error": "invalid token"}), 401
     
     token=auth.split(None, 1)[1]
     
     try:
         data=jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
     except:
-        return jsonify({"error": "invalid token"})
+        return jsonify({"error": "invalid token"}), 401
     
     return data["user_id"]
