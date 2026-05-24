c6b55fcb2b305af8ea92cc492c389598370832b3
Retr0 <azamatalihamroev3@gmail.com>
2026-05-25 04:28:35 +0500

    move auth routes into blueprint

--- app.py
+++ app.py
@@ -6,60 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 from db import cursor, users_db
 from auth import get_user_id
+from routes.auth_routes import auth_bp 
 app=Flask(__name__)
 SECRET_KEY=os.getenv("SECRET_KEY")
-
-#REGISTER
-@app.route("/register", methods=['POST'])
-def add_user():
-    data=request.get_json()
-    if not data:
-        return jsonify({"error": "json required"}), 400
-    if "username" not in data or "password" not in data:
-        return jsonify({"error": "username and password required"}), 400
-    password=data["password"]
-    hashed_password=bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-
-    cursor.execute(
-        "INSERT INTO users (username, password) VALUES (%s, %s)", 
-        (data["username"], hashed_password)
-    )
-
-    users_db.commit()
-
-    return jsonify({"message": "user created"}), 201
-
-#LOGIN
-@app.route('/login', methods=['POST'])
-def search_user():
-    data=request.get_json()
-    if not data:
-        return jsonify({"error": "json required"}), 400
-    if "username" not in data or "password" not in data:
-        return jsonify({"error": "username and password required"}), 400
-    password=data["password"]
-    cursor.execute(
-    "SELECT * FROM users WHERE username = %s",
-    (data["username"],)
-    )
-
-    user = cursor.fetchone()
-
-    if not user:
-        return jsonify({"error": "user not found"}), 404
-
-    if not bcrypt.checkpw(
-        password.encode(),
-        user["password"].encode()
-        ):
-        return jsonify({"error": "wrong password"}), 403
-    
-    payload = {
-    "user_id": user["id"]
-    }
-    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
-    return jsonify({"token": token}), 200
-
+app.register_blueprint(auth_bp)
 
 #TASK    
 @app.route("/tasks", methods=['POST'])
